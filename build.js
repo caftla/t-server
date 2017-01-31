3b860f274ef04a5b4e065e95fdbf0d257cacbdf8
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-31 10:30:40 +0100

    build script: create path id doesnt exist

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
 import Promise from 'bluebird'
 import R from 'ramda'
 import parse from './parser'
+import mkdirp from 'mkdirp'
 const fs = Promise.promisifyAll(require('fs'))
 const ejs = Promise.promisifyAll(require('ejs'))
 
 const pageDir = './pages'
 const pageBuildDir = './build/pages'
 
+mkdirp.sync(pageBuildDir)
 
 const buildPage = (page)=>
     R.composeP(
