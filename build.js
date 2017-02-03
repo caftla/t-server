a23031071274987c4e38cca02b126cd24cd62b72
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-02-03 11:37:26 +0100

    use babel to transform js to es5 to avoid accidentaly publishing es6 code

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 // @flow
-// @TODO: use babel to transform js to es5 to avoid accidentaly publishing es6 code
 
 import Promise from 'bluebird'
 import R from 'ramda'
 import parse from './parser'
 import mkdirp from 'mkdirp'
 import {minify} from 'uglify-js'
+import {transform} from 'babel-core'
 
 const fs = Promise.promisifyAll(require('fs'))
 const ejs = Promise.promisifyAll(require('ejs'))
@@ -16,16 +16,16 @@ const pageBuildDir = `${__dirname}/build/pages`
 // create build directory if doesnt exists
 mkdirp.sync(pageBuildDir)
 
-const jsMinify = (jsString: string): string => minify(jsString, {fromString: true, mangle: false}).code
+const babelify = (jsString: string): string => transform(jsString, {presets: ["es2015", "babili"]}).code
 
 
 const buildPage = (page: string): Promise =>
     R.composeP(
-        (it)=> fs.writeFileAsync(`${pageBuildDir}/${page}.html.js`, jsMinify(it)),
+        (it)=> fs.writeFileAsync(`${pageBuildDir}/${page}.html.js`, babelify(it)),
         ({js, css, html})=> ejs.renderFileAsync('./template.js', {tagContent: {
             js: R.map(({inline, content})=> {
                 // minify inline scripts
-                return {inline, content: inline ? jsMinify(content) : content}
+                return {inline, content: inline ? babelify(content) : content}
             })(js),
             css,
             html
