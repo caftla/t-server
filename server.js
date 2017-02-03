fcbcd729cdebc5f6af7f11fd90baad9ec18e539d
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-02-03 15:34:20 +0100

    unique reqId during user session

--- server.js
+++ server.js
@@ -23,18 +23,19 @@ const log = bunyan.createLogger({
                 method: req.method,
                 url: req.url,
                 headers: req.headers,
-                query: req.query
+                query: req.query,
+                params: req.params
             }
         }
     }
 })
 
 app.get('/webapi/v2/one-click-id', (req, res)=> {
-    const reqId = shortid.generate()
+    const reqId = req.query._req_id || shortid.generate()
 
     // create child logger with unqiue id, so subsecuent logs will have same req_id
     req.log = log.child({req_id: reqId})
-    req.log.info({eventType: 'visit', req})
+    req.log.info({req, eventType: 'webapi-visit'})
 
     const ipAddress = getClientIp(req)
     const payload = {
@@ -51,7 +52,14 @@ app.get('/webapi/v2/one-click-id', (req, res)=> {
         const {status, message, oneclickid} = data
         const jsonp = req.query.jsonp || 'doit'
 
-        req.log.info({eventType: 'api', eventArgs: {...payload, password: '...'}, response: data}, message)
+        req.log.info({
+            eventType: 'webapi-api-call',
+            eventArgs: {
+                url,
+                params: {...payload, password: '...'}
+            },
+            response: data
+        }, message)
 
         if (status !== 0) {
             res.status(400).send(message)
@@ -62,19 +70,31 @@ app.get('/webapi/v2/one-click-id', (req, res)=> {
         }
     })
     .catch((err)=> {
-        req.log.error({eventType: 'api', eventArgs: {...payload, password: '...'}, err})
+        req.log.error({
+            eventType: 'webapi-api-call',
+            eventArgs: {
+                url,
+                params: {...payload, password: '...'}
+            },
+            err
+        })
         res.sendStatus(400)
     })
 })
 
 
 app.get('/pages/:page', (req, res)=> {
+    const reqId = shortid.generate()
+
+    // create child logger with unqiue id, so subsecuent logs will have same req_id
+    req.log = log.child({req_id: reqId})
+    req.log.info({req, eventType: 'page-visit', eventArgs: {page: req.params.page}})
 
     fs.readFileAsync(`./build/pages/${req.params.page}.html.js`, 'utf8')
     .then((content)=> {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Content-Type', 'text/javascript')
-        res.send(`var queryStringObj=${JSON.stringify(req.query)}; \n ${content}`)
+        res.send(`var queryStringObj=${JSON.stringify({...req.query, _req_id: reqId})}; \n ${content}`)
     })
     .catch((err)=> {
         res.sendStatus(400)
