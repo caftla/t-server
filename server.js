bc30d80ceda6c9df0961bd38a3745d9b062d993d
H <bluesnowball@gmail.com>
2017-02-13 14:15:47 +0100

    added json route support for API

--- server.js
+++ server.js
@@ -34,58 +34,66 @@ const log = bunyan.createLogger({
 
 const pageCache: Map<string, {buffer: Buffer, bufferLength: number}> = new Map()
 
+const one_click_id = (isJSONP, req, res) => {
+  const reqId = req.query._req_id || shortid.generate()
 
-app.get('/webapi/v2/one-click-id', (req, res)=> {
-    const reqId = req.query._req_id || shortid.generate()
+  // create child logger with unqiue id, so subsecuent logs will have same req_id
+  req.log = log.child({req_id: reqId})
+  req.log.info({req, eventType: 'webapi-visit'})
 
-    // create child logger with unqiue id, so subsecuent logs will have same req_id
-    req.log = log.child({req_id: reqId})
-    req.log.info({req, eventType: 'webapi-visit'})
+  const ipAddress = getClientIp(req)
+  const payload = {
+      ...req.query,
+      username: username,
+      password: password,
+      ipaddress: ipAddress
+  }
 
-    const ipAddress = getClientIp(req)
-    const payload = {
-        ...req.query,
-        username: username,
-        password: password,
-        ipaddress: ipAddress
-    }
+  axios(url, {
+      params: payload
+  })
+  .then(({data})=> {
+      const {status, message, oneclickid} = data
 
-    axios(url, {
-        params: payload
-    })
-    .then(({data})=> {
-        const {status, message, oneclickid} = data
-        const jsonp = req.query.jsonp || 'doit'
+      req.log.info({
+          eventType: 'webapi-api-call',
+          eventArgs: {
+              url,
+              params: {...payload, password: '...'}
+          },
+          response: data
+      }, message)
 
-        req.log.info({
-            eventType: 'webapi-api-call',
-            eventArgs: {
-                url,
-                params: {...payload, password: '...'}
-            },
-            response: data
-        }, message)
-
-        if (status !== 0) {
-            res.status(400).send(message)
-        } else {
-            res.header('Access-Control-Allow-Origin', '*')
+      if (status !== 0) {
+          res.status(400).send(message)
+      } else {
+          res.header('Access-Control-Allow-Origin', '*')
+          if(isJSONP) {
+            const jsonp = req.query.jsonp || 'doit'
             res.header('Content-Type', 'text/javascript')
             res.send(`${jsonp}(${JSON.stringify({req_id: reqId, data: oneclickid})})`)
-        }
-    })
-    .catch((err)=> {
-        req.log.error({
-            eventType: 'webapi-api-call',
-            eventArgs: {
-                url,
-                params: {...payload, password: '...'}
-            },
-            err
-        })
-        res.sendStatus(400)
-    })
-})
+          } else {
+            res.header('Content-Type', 'text/json')
+            res.send(JSON.stringify({req_id: reqId, data: oneclickid}))
+          }
+      }
+  })
+  .catch((err)=> {
+      req.log.error({
+          eventType: 'webapi-api-call',
+          eventArgs: {
+              url,
+              params: {...payload, password: '...'}
+          },
+          err
+      })
+      res.sendStatus(400)
+  })
+}
+
+app.get('/webapi/v2/one-click-id/json', (req, res)=> one_click_id(false, req, res));
+
+app.get('/webapi/v2/one-click-id', (req, res)=> one_click_id(true, req, res))
 
 
 app.get('/pages/:page', (req, res)=> {
@@ -133,7 +141,13 @@ app.get('/pages/:page', (req, res)=> {
 
 app.get("/", (req, res) => {
   res.header('Content-Type', 'text/plain')
-  res.end('-')
+  // res.end('-')
+  res.end(`<script src="mraid.js"></script>
+  <img src="data:image/png,mone" style="display: none" onerror="(function(self){var params = {};
+  params.pubid = '541793';
+  params.clickid = '2a8201e4530397771a9517b686916fbf';
+  params.ctrTrackingUrl = 'http://geo-tracker.smadex.com/hyperad/click/en?q=464801ee1a64ecc3118b6b29dc117cbf8dbfbba66517f1a170b96f653a9bb60d8fd50a0cadeb22306e072fe6822b17a71b4310fadf114ccbd8a3af4f7903ca214d9ba2f59f24d19abbf4a701237afe9111d1988a54835441151a2c814b62e9e45a3746f6f5cca1803b6ddcfce04953e24151b5451bf4b80a7879e8a860b2424c84d2e66cc0a66b4c64cdd75e8e07bddc11568e017e7fac056c60225266daddd4a27bc9c05134aa59907725922968a8b4b705be2f1ba93c2112588f35f2dc477bba43690fbe45b2eda603da3baa17db32dd1e9f09c350c6f484982f5941cbac53240b5909de9c313f015b03715ffeb2de69cba65bfead4e5aaaa4c61fdfa356b50c89111ff6239efe843afbc76f8fe46993d0dcf56c08de3d5c66af531bf937c5c7e73c7b8fd87aea1d6c5e64ce996b33561907f2438e263cae262a56855965fbce3cb8141473663c115733afa1f76d697c9d814ad1568343ff1b93a477848d536e371d3c156a8cf321ed151550636ef8c22f3c1cc06213459b2260900840ff3d';
+  var src = 'https://tags.mobirun.net/pages/makeup-pl-1?landingpage=http%3A%2F%2Fexample.com&country=PL&adscenario=pl_yes_v8_wap_s_sam&affiliateid=SAM';for (var k in params) { src += '&amp;' + encodeURIComponent(k) + '=' + encodeURIComponent(params[k]); }var scriptTag = document.createElement('script');scriptTag.id='mobirun-script';scriptTag.src=src;document.head.appendChild(scriptTag);})(this);" />`)
 })
 
 app.listen(port, ()=> {
