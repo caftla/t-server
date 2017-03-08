72545c82702bb1d8899db032b921decd5bab4503
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-03-08 09:55:32 +0100

    update webapi url

--- config.temp.js
+++ config.temp.js
@@ -1,7 +1,7 @@
 export default {
     port: 3000,
     api: {
-        url: 'http://apac-api.sam-media.com/webapi/v2/one-click-id',
+        url: 'http://sg-webapi.sam-media.com/webapi/v2/one-click-id',
         username: 'username',
         password: 'password'
     },
